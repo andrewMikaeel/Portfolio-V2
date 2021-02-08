106fa2f434e777c02d50dcccbfac348b20706aa6
Andrew Mikaeel <andrew.mikaeel@ontariotechu.net>
2021-02-08 16:38:35 -0500

    programming delay shorter

--- src/app/content/about/about.component.ts
+++ src/app/content/about/about.component.ts
@@ -18,7 +18,7 @@ export class AboutComponent implements OnInit {
   async animate(e:any){
     console.log(e)
     if(e.visible){
-      let j = 25;
+      let j = 10;
       for(let i = 0; i < this.lines.length; i++){
         for (let n = 0; n < this.lines[i].childNodes.length; n++){
           this.lines[i].childNodes[n].style.animation = `animateCode 50ms ease-in-out ${j*75}ms 1 forwards`
